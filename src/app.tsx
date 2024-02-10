855dda848001b6d4878324a06fa0bf1ac1e6b3a3
Amaralcode19 <lucasamaralap@outlook.com>
2024-02-09 21:58:23 -0300

    ✅ finished

--- src/app.tsx
+++ src/app.tsx
@@ -1,31 +1,82 @@
 import logo from './assets/Logo.svg'
 import { NoteCard } from './components/note-card'
+import { NewNoteCard } from './components/new-note-card'
+import { ChangeEvent, useState } from 'react'
+
+interface Note {
+  id: string
+  date: Date
+  content: string
+}
 
 export function App() {
+  const [search, setSearch] = useState('')
+
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnStorage = localStorage.getItem('notes')
+
+    if(notesOnStorage){
+      return JSON.parse(notesOnStorage)
+    }
+    
+    return []
+  })
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    }
+
+    const notesArray = [newNote, ...notes]
+
+    setNotes([newNote, ...notes])
+
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
+  function onNoteDeleted(id: string){
+    const notesArray = notes.filter(note => {
+      return note.id != id
+    })
+
+    setNotes(notesArray)
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+  
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value
+
+    setSearch(query)
+  }
+
+  const filteredNotes = search != '' 
+  ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())) 
+  : notes
+
  return (
-    <div className="mx-auto max-w-6xl my-10 space-y-6">
+    <div className="mx-auto max-w-6xl my-10 space-y-6 px-5">
       <img src={logo} alt="nlw-expert" />
 
       <form action="w-full">
         <input 
           type="text" 
-          placeholder='Busque suas notas' 
+          placeholder='Browse in your notes...' 
           className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'
+          onChange={handleSearch}
           />  
       </form>     
 
       <div className='h-px bg-slate-700'/>
 
-      <div className='grid grid-cols-3  gap-6 auto-rows-[258px] overflow-hidden relative'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px] relative'>
+        <NewNoteCard onNoteCreated={onNoteCreated}/>
 
-        <div className='rounded-md p-5 bg-slate-700 space-y-3'>
-          <span className='text-sm font-medium text-slate-200'>Adicionar nota</span>
-          <p className='text-sm leading-6 text-slate-400'>Grave uma nota em áudio que será convertida para texto automaticamente.</p>
-        </div>
+        {filteredNotes.map(note => {
+          return <NoteCard onNoteDeleted={onNoteDeleted} key={note.id} note={note}/>
+        })}
 
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
       </div> 
     </div>
   )
--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,6 +1,155 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import { X } from 'lucide-react'
+import { useState, ChangeEvent, FormEvent } from 'react'
+import toast from 'react-hot-toast'
 
-export function newNoteCard() {
+interface NewNoteCardProps {
+  onNoteCreated: (content: string) => void
+}
+
+
+let speechRecognition: SpeechRecognition | null = null
+
+export function NewNoteCard({onNoteCreated}: NewNoteCardProps) {
+  const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true)
+  const [isRecording, setIsRecording] = useState(false)
+  const [content, setContent] = useState('')
+
+  function handleStartEditor() {
+    setShouldShowOnboarding(false)
+  }
+
+  function handleStartRecording() {
+    setIsRecording(true)
+
+    const isSpeechRecognitionAPIAvaliable ='SpeechRecognition' in window
+      || 'webkitSpeechRecognition' in window
+
+    if(!isSpeechRecognitionAPIAvaliable){
+      alert('Infelizmente o seu navegador não suporta a API de gravação!')
+      return
+    }
+
+    setIsRecording(true)
+    setShouldShowOnboarding(false)
+
+    const SpeechRecognitionAPI = window.SpeechRecognition || window.webkitSpeechRecognition
+
+    speechRecognition = new SpeechRecognitionAPI()
+
+    speechRecognition.lang = 'pt-BR'
+    speechRecognition.continuous = true
+    speechRecognition.maxAlternatives = 1
+    speechRecognition.interimResults = true
+
+    speechRecognition.onresult = (event) => {
+      console.log(event.results)
+    }
+
+    speechRecognition.onresult = (event) => {
+      const transcription = Array.from(event.results).reduce((text, result) => {
+        return text.concat(result[0].transcript)
+      }, '')
+
+      setContent(transcription)
+    }
+
+    speechRecognition.start()
+  }
+
+  function handleStopRecording() {
+    setIsRecording(false)
+   
+    if(speechRecognition != null){
+      speechRecognition.stop()
+    }
+    
+  }
+
+  function handleContentChanged(event: ChangeEvent<HTMLTextAreaElement>){
+    setContent(event.target.value)
+
+    if (event.target.value === '') {
+      setShouldShowOnboarding(true)
+    }
+
+  }
+  
+  function handleSaveNote(event: FormEvent) {
+    event.preventDefault()
+
+    if(content === ''){
+      return
+    }
+    
+    onNoteCreated(content)
+
+    setContent('')
+    setShouldShowOnboarding(true)
+    toast.success("note created with sucess!")
+  }
+
   return (
-    <div>new-note-card</div>
+    <Dialog.Root>
+      <Dialog.Trigger className='rounded-md flex flex-col bg-slate-700 text-left p-5 gap-3 outline-none hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
+          <span className='text-sm font-medium text-slate-500'>
+            Add note
+          </span>
+          <p className='text-sm leading-6 text-slate-400'>
+            Record one note in audio what will be converted to text automatically
+          </p>
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className='inset-0 fixed bg-black/50' />
+        <Dialog.Content className='fixed overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60dvh] bg-slate-700 md:rounded-md flex flex-col outline-none' >
+          <Dialog.Close className='absolute top-0 right-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100' >
+            <X className='size-5' />
+          </Dialog.Close>
+          <form onSubmit={handleSaveNote} className='flex-1 flex flex-col'>
+            <div className='flex flex-1 flex-col gap-3 p-5'>
+              <span className='text-sm font-medium  text-slate-200'>
+                Add note
+              </span>
+              {shouldShowOnboarding ? (
+                <p className='text-sm leading-6 text-slate-400'>
+                start <button onClick={handleStartRecording} className='text-lime-400 font-medium hover:underline'>recording your note</button> in audio or if you prefer <button onClick={handleStartEditor} className='text-lime-400 font-medium hover:underline'>just type</button>
+              </p>) : (
+              <p>
+                <textarea
+                  autoFocus
+                  className='text-m leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none'
+                  onChange={handleContentChanged}
+                  value={content}
+                >
+                  
+                </textarea>
+              </p>
+              )
+              }
+            </div>
+
+            {isRecording ? (
+              <button 
+                type='button' 
+                onClick={handleStopRecording}
+                className='w-full flex items-center justify-center gap-2 bg-slate-900 py-4 text-center text-sm text-slate-350 outline-none font-medium group hover:text-slate-100'
+                >
+                <div className='size-3 rounded-full bg-red-500 animate-bounce'/>
+                Recording! (clique p/ interromper)
+              </button>
+            ) : (
+              <button 
+                  type='button' 
+                  onClick={handleSaveNote}
+                  className='w-full bg-lime-400 py-4 text-center text-sm text-slate-950 outline-none font-medium group '
+                  >
+                  Save note
+              </button>
+            )}
+          </form>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   )
 }
